03c1ecdf0372ba3fb4220876acb210e99bc05670
fatimememmedli <bd7u8hqcl@code.edu.az>
2023-10-12 19:47:59 +0400

    created file

---
+++ array task/app.js
@@ -0,0 +1,38 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let sum = 0;
+// for (i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum / arr.length);
+
+// // task 2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let min = arr[0];
+// let max = arr[1];
+// let sum = 0;
+// for (i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+//   sum = max + min;
+// }
+// console.log(sum / 2);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let num = 0;
+// let sade = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 1; j <= a[i]; j++) {
+//     if (a[i] % j == 0) {
+//       num++;
+//     }
+//   }
+//   if (num <= 2) {
+//     sade++;
+//   }
+// }
+// console.log(sade);
---
+++ for-while-tasks/app.js
@@ -0,0 +1,51 @@
+// let num = Number(prompt("ededi daxil edin"));
+// let word = prompt("sozu daxil edin");
+
+// for (let i = 1; i < num + 1; i++) {
+//   console.log(word);
+// }
+//task2
+// let num = Number(prompt("ededi daxil edin"));
+// let fac = 1;
+// for (i = 1; i < num + 1; i++) {
+//   fac = fac * i;
+// }
+
+// alert(`${fac}`);
+// task3
+// let num1 = Number(prompt("eded 1:"));
+// let num2 = Number(prompt("eded 2:"));
+// let controller = true;
+// let sum = 0;
+// do {
+//   if (num1 < num2) {
+//     for (let i = num1; i < num2 + 1; i++) {
+//       sum = sum + i;
+//       controller = false;
+//     }
+//     alert(sum);
+//   } else {
+//     num2 = Number(prompt("yeniden daxil edin:"));
+//   }
+// } while (controller);
+
+// task5
+
+// let str = prompt("sozleri daxil edin");
+// let num = 0;
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i] == ",") {
+//     num += 1;
+//   }
+// }
+// alert(num + 1);
+
+// task-6
+
+// let num = Number(prompt("number;"));
+// let pow = Number(prompt("power;"));
+// let result = 1;
+// for (let i = 1; i < pow + 1; i++) {
+//   result = result * num;
+// }
+// alert(result);
